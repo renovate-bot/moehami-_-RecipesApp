b8d884d0c307bf78bc0ef13ee01b30c86f114efc
Micka <mickael.murmann@gmail.com>
2024-09-10 16:25:02 +0200

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -10,4 +10,7 @@ declare global {
 export const db = globalThis.prisma || new PrismaClient();
 
 // If not in production environment, assigning the db instance to the global prisma variable
-if(process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+    // Use `let` here instead of `var` to declare a global variable
+    globalThis.prisma = db;
+}
