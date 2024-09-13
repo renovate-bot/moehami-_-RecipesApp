7e96067043fc7936e9c6901ae54638979bcaf093
Micka <mickael.murmann@gmail.com>
2024-09-13 23:51:41 +0200

    Fix Navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ import ThemeSwitcher from './ThemeSwitcher';
 
 const NavBar = () => {
   return (
-    <nav className='flex gap-5 items-center py-6 px-6 md:px-[50px] lg:px-[100px] h-[100px] bg-slate-800 w-full'>
+    <nav className='flex h-auto gap-5 items-center py-6 px-6 md:px-[50px] lg:px-[100px] bg-slate-800 w-full'>
         <Link className='text-white underline underline-offset-4' href='/recipe'>Recipes</Link>
         <Link className='text-white underline underline-offset-4' href='/search'>Search</Link>
         <ThemeSwitcher />
