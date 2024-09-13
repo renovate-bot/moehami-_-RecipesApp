d51fab8cf89634b2558cfeb08c7e300476764274
Micka <mickael.murmann@gmail.com>
2024-09-13 16:13:19 +0200

    Adjust Swiper CSS

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -48,45 +48,47 @@ const RecipesPage = () => {
         <div className='pb-5'>
             <h1 className='text-4xl font-bold mb-5'>Last Recipes</h1>
 
-            <Swiper
-                className='p-6'
-                modules={[EffectCoverflow, Mousewheel, Pagination, Autoplay]}
-                effect={'coverflow'}
-                autoplay={{
-                    delay: 2500,
-                    pauseOnMouseEnter: true,
-                    disableOnInteraction: false
-                }}
-                spaceBetween={50}
-                mousewheel={true}
-                // slidesPerView={'auto'}
-                loop={shouldEnableLoop}
-                pagination={{clickable: true}}
-                centeredSlides={true}
-                grabCursor={true}
-                coverflowEffect={{
-                    rotate: 15,
-                    // stretch: 0,
-                    modifier: 1,
-                    slideShadows: false
-                }}
-                breakpoints={{
-                    0: {
-                        slidesPerView: 1
-                    },
-                    600: {
-                        slidesPerView: 2
-                    },
-                    850: {
-                        slidesPerView: 3
-                    },
-                    1285: {
-                        slidesPerView: 4
-                    },
-                }}
-            >
-                {recipes.length > 0 ? swiperSlides : <p>No recipes found</p>}
-            </Swiper>            
+            <div className='swiper-container'>
+                <Swiper
+                    className='p-6'
+                    modules={[EffectCoverflow, Mousewheel, Pagination, Autoplay]}
+                    effect={'coverflow'}
+                    autoplay={{
+                        delay: 2500,
+                        pauseOnMouseEnter: true,
+                        disableOnInteraction: false
+                    }}
+                    spaceBetween={50}
+                    mousewheel={true}
+                    // slidesPerView={'auto'}
+                    loop={shouldEnableLoop}
+                    pagination={{clickable: true}}
+                    centeredSlides={true}
+                    grabCursor={true}
+                    coverflowEffect={{
+                        rotate: 15,
+                        // stretch: 0,
+                        modifier: 1,
+                        slideShadows: false
+                    }}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1
+                        },
+                        600: {
+                            slidesPerView: 2
+                        },
+                        850: {
+                            slidesPerView: 3
+                        },
+                        1285: {
+                            slidesPerView: 4
+                        },
+                    }}
+                >
+                    {recipes.length > 0 ? swiperSlides : <p>No recipes found</p>}
+                </Swiper>            
+            </div>
         </div>
     )
 }
