f9e3365b70c3f9401756dc7fac60ca5567179e61
Micka <mickael.murmann@gmail.com>
2024-09-16 20:11:06 +0200

    Responsive menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,28 +1,13 @@
 "use client"
 
-// import Link from 'next/link';
-// import React from 'react'
-// import ThemeSwitcher from './ThemeSwitcher';
-
-// const NavBar = () => {
-//   return (
-//     <nav className='flex h-auto gap-5 items-center py-6 px-6 md:px-[50px] lg:px-[100px] bg-slate-800 w-full'>
-//         <Link className='text-white underline underline-offset-4' href='/recipe'>Recipes</Link>
-//         <Link className='text-white underline underline-offset-4' href='/search'>Search</Link>
-//         <ThemeSwitcher />
-//     </nav>
-//   )
-// }
-
-// export default NavBar;
-
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import ThemeSwitcher from './ThemeSwitcher';
+import { Menu, X } from 'lucide-react';
 
 const NavBar = () => {
-
   const [activeLink, setActiveLink] = useState<string>('/');
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   useEffect(() => {
     setActiveLink(window.location.pathname);
@@ -30,45 +15,65 @@ const NavBar = () => {
 
   const handleLinkClick = (path: string) => {
     setActiveLink(path);
+    setIsMenuOpen(false);
   };
-  
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className='flex justify-between items-center py-6 px-6 md:px-[50px] lg:px-[100px] bg-slate-800 w-full shadow-lg'>
+    <nav className='flex z-50 justify-between items-center py-6 px-6 md:px-[50px] lg:px-[100px] bg-slate-800 w-full shadow-lg relative'>
       {/* Logo/Branding */}
       <div className='text-2xl font-bold text-[#f26b5a]'>
         <Link 
           href='/' 
           className='hover:text-[#f78b6d] transition duration-300' 
-          onClick={() => handleLinkClick('/')}>
+          onClick={() => handleLinkClick('/')}
+        >
           MyRecipes
         </Link>
       </div>
 
-      {/* Navigation Links */}
-      <div className='flex gap-8'>
-        <Link 
-          href='/recipe'
-          className={`text-lg font-medium hover:text-[#f78b6d] transition duration-300 ${activeLink === '/recipe' ? 'text-[#f78b6d]' : 'text-white'} hover:text-[#f78b6d]'}`}
-          aria-current="page"
-          onClick={() => handleLinkClick('/recipe')}
-        >
-            Recipes
-        </Link>
-        <Link 
-          href='/search'
-          className={`text-lg font-medium hover:text-[#f78b6d] transition duration-300 ${activeLink === '/search' ? 'text-[#f78b6d]' : 'text-white'} hover:text-[#f78b6d]'}`}
-          onClick={() => handleLinkClick('/search')}
-        >
-            Search
-        </Link>
+      {/* Burger menu button (visible on small screens) */}
+      <div className='md:hidden flex items-center'>
+        <button onClick={toggleMenu} className='text-white focus:outline-none'>
+          {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
       </div>
 
-      {/* Theme Switcher */}
-      <ThemeSwitcher />
+      {/* Navigation Links with animation */}
+      <div
+        className={`md:flex gap-8 absolute md:static top-[75px] left-0 w-full md:w-auto bg-slate-800 md:bg-transparent transition-transform duration-300 ease-in-out ${
+          isMenuOpen
+            ? 'transform translate-x-0 opacity-100'
+            : 'transform -translate-x-full opacity-0 md:opacity-100 md:translate-x-0'
+        }`}
+      >
+        <div className='flex items-center flex-col md:flex-row md:gap-8 p-4 md:p-0'>
+          <Link
+            href='/recipe'
+            className={`block md:inline-block text-lg font-medium hover:text-[#f78b6d] transition duration-300 ${activeLink === '/recipe' ? 'text-[#f78b6d]' : 'text-white'}`}
+            onClick={() => handleLinkClick('/recipe')}
+          >
+            Recipes
+          </Link>
+          <Link
+            href='/search'
+            className={`block md:inline-block text-lg font-medium hover:text-[#f78b6d] transition duration-300 ${activeLink === '/search' ? 'text-[#f78b6d]' : 'text-white'}`}
+            onClick={() => handleLinkClick('/search')}
+          >
+            Search
+          </Link>
+
+          {/* Theme Switcher visible in mobile and desktop */}
+          <div className='mt-4 md:mt-0'>
+            <ThemeSwitcher />
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
 
 export default NavBar;
-
-
