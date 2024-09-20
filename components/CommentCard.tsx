362a585ad573e4afff2ffe3c6dd947d2f12897a5
Micka <mickael.murmann@gmail.com>
2024-09-21 00:26:29 +0200

    Fix CSS (CommentCard)

--- components/CommentCard.tsx
+++ components/CommentCard.tsx
@@ -68,7 +68,7 @@ const CommentCard = ({ comment, onDelete }: CommentCardProps) => {
                 ) : (
                     <UserCircleIcon className="h-8 w-8 text-custom-orange/60" />
                 )}
-                    <p className="font-semibold text-lg">{comment?.username}</p>
+                    <p className="font-semibold text-sm">{comment?.username}</p>
                 </div>
                 <p className="text-slate-500 dark:text-slate-400 text-sm mb-2">
                     {new Date(comment.createdAt).toLocaleDateString()} at {new Date(comment.createdAt).toLocaleTimeString()}
