7e66b8c488defff034a92cf544cea0e7fe5cdf4b
Micka <mickael.murmann@gmail.com>
2024-09-21 00:29:06 +0200

    Fix CSS CommentForm

--- components/CommentForm.tsx
+++ components/CommentForm.tsx
@@ -61,7 +61,7 @@ const CommentForm: React.FC<CommentFormProps> = ({ onSubmit }) => {
 
             <button
                 type="submit"
-                className="self-start bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700 transition duration-300"
+                className="self-start bg-custom-orange/80 text-white px-4 py-2 rounded-lg hover:bg-custom-orange transition duration-300"
             >
                 Post Comment
             </button>
