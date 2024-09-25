931be9aeb4259a4f0d433224463b20551d21a701
Micka <mickael.murmann@gmail.com>
2024-09-25 16:39:10 +0200

    Fix CSS (Meal planner page)

--- app/mealplanner/page.tsx
+++ app/mealplanner/page.tsx
@@ -67,7 +67,7 @@ const MealPlanner = () => {
     };
 
     return (
-        <div className="mx-auto p-6 dark:bg-gray-800">
+        <div className="mx-auto p-6">
             <h1 className="text-3xl font-bold mb-6 text-gray-900 dark:text-white">
                 Meal Planner
             </h1>
@@ -94,9 +94,9 @@ const MealPlanner = () => {
                 />
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-4">
                 {mealPlans.map((mealPlan) => (
-                    <div className="shadow-md rounded-lg p-4" key={mealPlan.mealType}>
+                    <div className="shadow-md rounded-lg" key={mealPlan.mealType}>
                         <MealSection
                             mealType={mealPlan.mealType}
                             onRecipesUpdate={handleRecipesUpdate}
