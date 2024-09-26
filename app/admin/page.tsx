746a2f2a9daeb6f4c99dbaf7abfbc8957acab224
Micka <mickael.murmann@gmail.com>
2024-09-26 22:13:59 +0200

    Include list of recipes in admin dashboard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { HomeIcon, LogOutIcon, Menu, SettingsIcon, UsersIcon, X } from 'lucide-react';
+import { RecipeType } from '@/types/types';
+import { CookingPotIcon, HomeIcon, LogOutIcon, Menu, SettingsIcon, UsersIcon, X } from 'lucide-react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
 
 // Define User Type
@@ -14,13 +16,20 @@ interface User {
 }
 
 interface ApiResponse {
-    data: User[]; // Array of User objects
-    totalCount: number; // Number of users
+    data: User[]; 
+    totalCount: number; 
 }
 
 // Users Component receives users, loading, and error as typed props
 interface UsersProps {
-    users: User[]; // Array of User objects
+    users: User[]; 
+    loading: boolean;
+    error: string;
+}
+
+// Users Component receives recipes, loading, and error as typed props
+interface RecipesProps {
+    recipes: RecipeType[]; 
     loading: boolean;
     error: string;
 }
@@ -33,9 +42,9 @@ const Users = ({ users, loading, error }: UsersProps) => {
         <div className="bg-white dark:bg-gray-800 p-6 shadow rounded-lg">
             <h2 className="text-xl font-semibold mb-4 dark:text-gray-100">User Management</h2>
             <p className="text-gray-600 dark:text-gray-300">Here you can manage all your platform's users.</p>
-            <div>
-                {users.length > 0 ? ( // Check if users exist
-                    <table className="mt-8 min-w-full bg-white dark:bg-gray-800 shadow-md rounded-lg overflow-hidden">
+            <div className="overflow-x-auto"> {/* Make the table container responsive */}
+                {users.length > 0 ? (
+                    <table className="my-8 min-w-full bg-white dark:bg-gray-800 shadow-md rounded-lg overflow-hidden">
                         <thead className="bg-gray-200 dark:bg-gray-700">
                             <tr>
                                 <th className="px-6 py-3 text-gray-600 dark:text-gray-300 font-semibold text-left text-sm uppercase">Avatar</th>
@@ -46,9 +55,11 @@ const Users = ({ users, loading, error }: UsersProps) => {
                         <tbody>
                             {users.map((user) => (
                                 <tr key={user.id} className="hover:bg-gray-100 dark:hover:bg-gray-600">
-                                    <td className="border-b border-gray-200 dark:border-gray-600 px-6 py-4"><Image src={user.imageUrl} alt={user.id} width={50} height={50} className="w-[50px] h-[50px] rounded-full object-cover" /></td>
-                                    <td className="border-b border-gray-200 dark:border-gray-600 px-6 py-4">{user.emailAddresses[0]?.emailAddress || 'N/A'}</td>
-                                    <td className="border-b border-gray-200 dark:border-gray-600 px-6 py-4">{user.firstName} {user.lastName}</td>
+                                    <td className="border-b border-gray-200 dark:border-gray-600 px-4 py-4">
+                                        <Image src={user.imageUrl} alt={user.id} width={50} height={50} className="w-[50px] h-[50px] rounded-full object-cover" />
+                                    </td>
+                                    <td className="border-b border-gray-200 dark:border-gray-600 px-4 py-4">{user.emailAddresses[0]?.emailAddress || 'N/A'}</td>
+                                    <td className="border-b border-gray-200 dark:border-gray-600 px-4 py-4">{user.firstName} {user.lastName}</td>
                                 </tr>
                             ))}
                         </tbody>
@@ -68,6 +79,75 @@ const Dashboard = () => (
     </div>
 );
 
+const Recipes = ({ recipes, loading, error }: RecipesProps) => {
+    const router = useRouter();
+
+    if (loading) return <div>Loading recipes...</div>;
+    if (error) return <div>{error}</div>;
+
+    const handleEdit = (id: string) => {
+        console.log(`Edit recipe with ID: ${id}`);
+    };
+
+    const handleDelete = (id: string) => {
+        console.log(`Delete recipe with ID: ${id}`);
+    };
+
+    const handleCreate = () => {
+        router.push('/recipe/add');
+    };
+
+    return (
+        <div className="bg-white dark:bg-gray-800 p-6 shadow rounded-lg">
+            <h2 className="text-xl font-semibold mb-4 dark:text-gray-100">Manage Recipes</h2>
+            <p className="text-gray-600 dark:text-gray-300 mb-4">Here you can manage your recipes.</p>
+            <button
+                onClick={handleCreate}
+                className="mb-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+            >
+                Create Recipe
+            </button>
+            <div className="overflow-x-auto"> {/* Make the table container responsive */}
+                {recipes.length > 0 ? (
+                    <table className="min-w-full bg-white dark:bg-gray-800 shadow-md rounded-lg overflow-hidden">
+                        <thead className="bg-gray-200 dark:bg-gray-700">
+                            <tr>
+                                <th className="px-6 py-3 text-gray-600 dark:text-gray-300 font-semibold text-left text-sm uppercase">Title</th>
+                                <th className="px-6 py-3 text-gray-600 dark:text-gray-300 font-semibold text-left text-sm uppercase">Category</th>
+                                <th className="px-6 py-3 text-gray-600 dark:text-gray-300 font-semibold text-left text-sm uppercase">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {recipes.map((recipe) => (
+                                <tr key={recipe.id} className="hover:bg-gray-100 dark:hover:bg-gray-600">
+                                    <td className="border-b border-gray-200 dark:border-gray-600 px-4 py-4">{recipe.title}</td>
+                                    <td className="border-b border-gray-200 dark:border-gray-600 px-4 py-4">{recipe.category.name}</td>
+                                    <td className="border-b border-gray-200 dark:border-gray-600 px-4 py-4">
+                                        <button
+                                            onClick={() => handleEdit(recipe.id)}
+                                            className="text-yellow-500 hover:text-yellow-600"
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            onClick={() => handleDelete(recipe.id)}
+                                            className="ml-4 text-red-500 hover:text-red-600"
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <p>No recipes found.</p>
+                )}
+            </div>
+        </div>
+    );
+};
+
 const Settings = () => (
     <div className="bg-white dark:bg-gray-800 p-6 shadow rounded-lg">
         <h2 className="text-xl font-semibold mb-4 dark:text-gray-100">Settings</h2>
@@ -77,46 +157,86 @@ const Settings = () => (
 
 const AdminDashboardPage = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [activeTab, setActiveTab] = useState<'dashboard' | 'users' | 'settings'>('dashboard');
-    const [users, setUsers] = useState<User[]>([]); // Initialize as an empty array
+    const [activeTab, setActiveTab] = useState<'dashboard' | 'users' | 'settings' | 'recipes'>('dashboard');
+    const [users, setUsers] = useState<User[]>([]);
+    const [recipes, setRecipes] = useState<RecipeType[]>([]); 
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
 
     useEffect(() => {
         const fetchUsers = async () => {
-            if (activeTab !== 'users') return; // Only fetch when activeTab is 'users'
-            setLoading(true);
-            setError('');
+            if (activeTab === 'users') {
+                setLoading(true);
+                setError('');
 
-            try {
-                const response = await fetch('/api/users');
-                if (!response.ok) throw new Error('Failed to fetch users');
-                
-                const data: ApiResponse = await response.json();
-                console.log(data);
-                setUsers(data.data); // Set users to the array of User objects
-            } catch (error) {
-                console.error('Error fetching users:', error);
-                setError('Failed to load users');
-            } finally {
-                setLoading(false);
+                try {
+                    const response = await fetch('/api/users');
+                    if (!response.ok) throw new Error('Failed to fetch users');
+                    
+                    const data: ApiResponse = await response.json();
+                    console.log(data);
+                    setUsers(data.data);
+                } catch (error) {
+                    console.error('Error fetching users:', error);
+                    setError('Failed to load users');
+                } finally {
+                    setLoading(false);
+                }
+            }
+
+            if(activeTab === 'recipes') {
+                setLoading(true);
+                setError('');
+
+                try {
+                    const response = await fetch('/api/recipe');
+                    if (!response.ok) throw new Error('Failed to fetch recipes');
+                    
+                    const data: RecipeType[] = await response.json();
+                    setRecipes(data);
+                } catch (error) {
+                    console.error('Error fetching recipes:', error);
+                    setError('Failed to load users');
+                } finally {
+                    setLoading(false);
+                }
             }
         };
 
         fetchUsers();
     }, [activeTab]);
 
+    useEffect(() => {
+        // Adjust the sidebar based on screen width
+        const handleResize = () => {
+            if (window.innerWidth < 768) {
+                setIsSidebarOpen(false); // Close sidebar on small screens
+            } else {
+                setIsSidebarOpen(true); // Open sidebar on larger screens
+            }
+        };
+
+        // Set initial sidebar state based on current screen size
+        handleResize();
+        // Attach resize event listener
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize); // Cleanup listener
+        };
+    }, []);
+
     const toggleSidebar = () => {
         setIsSidebarOpen((prev) => !prev);
     };
 
-    // Render the selected content based on the active tab
     const renderContent = () => {
         switch (activeTab) {
             case 'users':
                 return <Users users={users} loading={loading} error={error} />;
             case 'settings':
                 return <Settings />;
+            case 'recipes':
+                return <Recipes recipes={recipes} loading={loading} error={error} />;
             default:
                 return <Dashboard />;
         }
@@ -124,13 +244,11 @@ const AdminDashboardPage = () => {
 
     return (
         <div className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
-            {/* Sidebar */}
             <div
                 className={`${
                     isSidebarOpen ? "w-64" : "w-20"
-                } bg-slate-200 dark:bg-gray-800 transition-all duration-300 dark:text-gray-100 flex flex-col min-h-screen`}
+                } bg-slate-200 dark:bg-gray-800 transition-all duration-300 dark:text-gray-100 md:flex flex-col min-h-screen`}
             >
-                {/* Sidebar Header */}
                 <div className="flex items-center justify-between p-4 dark:text-white bg-slate-300 dark:bg-gray-700">
                     <span className={`text-2xl font-bold ${!isSidebarOpen && "hidden"}`}>
                         Admin
@@ -140,9 +258,8 @@ const AdminDashboardPage = () => {
                     </button>
                 </div>
 
-                {/* Sidebar Links */}
                 <nav className="flex-1 p-4 space-y-4">
-                    {['dashboard', 'users', 'settings'].map(tab => (
+                    {['dashboard', 'users', 'settings', 'recipes'].map(tab => (
                         <button
                             key={tab}
                             onClick={() => setActiveTab(tab as 'dashboard' | 'users' | 'settings')}
@@ -153,12 +270,12 @@ const AdminDashboardPage = () => {
                             {tab === 'dashboard' && <HomeIcon className="w-6 h-6" />}
                             {tab === 'users' && <UsersIcon className="w-6 h-6" />}
                             {tab === 'settings' && <SettingsIcon className="w-6 h-6" />}
+                            {tab === 'recipes' && <CookingPotIcon className="w-6 h-6" />}
                             <span className={`${!isSidebarOpen && "hidden"}`}>{tab.charAt(0).toUpperCase() + tab.slice(1)}</span>
                         </button>
                     ))}
                 </nav>
 
-                {/* Sidebar Footer */}
                 <div className="p-4">
                     <button className="flex items-center space-x-2 text-red-500 hover:bg-red-100 dark:hover:bg-red-100 rounded-lg p-2 w-full">
                         <LogOutIcon className="w-6 h-6" />
@@ -167,16 +284,13 @@ const AdminDashboardPage = () => {
                 </div>
             </div>
 
-            {/* Main Content */}
             <div className="flex-1 flex flex-col">
-                {/* Header */}
                 <header className="flex items-center justify-between p-6 bg-white dark:bg-gray-800 shadow">
                     <h1 className="text-2xl font-bold dark:text-gray-100">Admin Dashboard</h1>
                 </header>
 
-                {/* Main Content Area */}
                 <main className="flex-1 p-6 bg-gray-100 dark:bg-gray-900">
-                    {renderContent()} {/* Render content based on the active tab */}
+                    {renderContent()}
                 </main>
             </div>
         </div>
