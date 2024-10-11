3c46e0c1e266c08bec914255f66382b01063eb37
moehami <m.aljaaly@yahoo.co.uk>
2024-10-11 22:54:50 +0300

    Update SectionHeader.tsx

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -10,7 +10,7 @@ const SectionHeader = ({ icon: Icon, title, count }: SectionHeaderProps) => {
     return (
         <div className='flex space-x-4 items-center mt-8 mb-5 text-custom_orange font-semibold'>
             <Icon className="" />
-            <h2 className='text-xl'>{title}</h2>
+            <h2 className='text-xl'>{title} MOe</h2>
             {count !== undefined && count !== null && <p className=''>({count})</p>}
         </div>
     );
